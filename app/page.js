b39d60440c2a2401826219453773b9e53ea4326b
AdarshVMore <skullcrushermore@gmail.com>
2023-12-09 03:23:12 +0530

    had to push these small commit in order to pull an important commit

--- app/page.js
+++ app/page.js
@@ -1,8 +1,9 @@
-'use client'
-import {useRouter} from 'next/navigation';
-const ClientId = process.env.CLIENT_ID
+"use client";
 
-import Image from 'next/image'
+import { useRouter } from "next/navigation";
+const ClientId = process.env.NEXT_PUBLIC_CLIENT_ID;
+
+import Image from "next/image";
 
 import {
   ConnectWallet,
@@ -16,76 +17,74 @@ import {
   metamaskWallet,
   useContract,
   useMetadata,
-  Web3Button
+  Web3Button,
 } from "@thirdweb-dev/react";
 
-import { customSmartWallet, customSmartWallet2 } from "./constants/walletConfig"
+import {
+  customSmartWallet,
+  customSmartWallet2,
+} from "./constants/walletConfig";
 
 export default function Home() {
   return (
     <ThirdwebProvider
       activeChain={"polygon"}
       clientId={ClientId}
-      supportedWallets={[
-        customSmartWallet,
-        customSmartWallet2,
-      ]}
+      supportedWallets={[customSmartWallet, customSmartWallet2]}
     >
       <Landing />
     </ThirdwebProvider>
-  )
+  );
 }
 
 const Landing = () => {
-
-  const address = useAddress()
-  const router = useRouter()
+  const address = useAddress();
+  const router = useRouter();
 
   //Connect Wallet Theme
   const customeTheme = darkTheme({
     colors: {
-      modalBg: "linear-gradient(to right, rgb(255, 228, 230), rgb(204, 251, 241))",
+      modalBg:
+        "linear-gradient(to right, rgb(255, 228, 230), rgb(204, 251, 241))",
       primaryText: "#555",
       secondaryText: "#333333",
       walletSelectorButtonHoverBg: "rgb(194, 207, 204)",
       separatorLine: "#888",
       borderColor: "#333333",
-    }
-  })
+    },
+  });
 
   return (
     <div>
-
-      {address ?
+      {address ? (
         //Wallet Connected Page (Marketplace)
         <ConnectWallet />
-
-        :
+      ) : (
         //Wallet Not Connected (Landing Page)
         <main className="flex h-screen items-center justify-center bg-gradient-to-r from-yellow-200 via-yellow-400 to-yellow-700">
           <ConnectWallet
             theme={customeTheme}
             modalSize={"wide"}
             btnTitle="ENTER GAME"
-
             modalTitle="GameFT"
             modalTitleIconUrl=""
-
             welcomeScreen={() => {
               return (
                 <div>
                   <MediaRenderer
-                    src={"ipfs://QmT5ULF1ZQpsKUHmnWk7yBoPCnSYLQckAR9VRcEopFBiSN/game-box-gif.gif"}
+                    src={
+                      "ipfs://QmT5ULF1ZQpsKUHmnWk7yBoPCnSYLQckAR9VRcEopFBiSN/game-box-gif.gif"
+                    }
                     height={570}
                     width={570}
                     style={{ objectFit: "fill", objectPosition: "right" }}
                   />
                 </div>
-              )
+              );
             }}
           />
         </main>
-      }
+      )}
     </div>
-  )
-}
+  );
+};
