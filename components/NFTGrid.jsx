aa64c2010c0bc492f1398c06348d2f38611395a0
shubham_wadne <kaylekashubham@gmail.com>
2023-12-09 07:55:44 +0530

    Added marketplace section for buying NFTs

---
+++ components/NFTGrid.jsx
@@ -0,0 +1,45 @@
+import NFTComponent from './NFT'
+import Link from 'next/link'
+import { NFT_DROP_ADDRESS } from "../app/constants/constant";
+
+
+export default function NFTGrid({
+  isLoading,
+  data,
+  overrideOnclickBehavior,
+  emptyText = 'No NFTs Found',
+}) {
+    const contractAddress = NFT_DROP_ADDRESS
+
+  return (
+    <div>
+      {isLoading ? (
+        <h2>Loading.....</h2>
+      ) : data && data.length > 0 ? (
+        <div className="grid grid-cols-4 gap-4">
+        {data.map((nft) =>
+          !overrideOnclickBehavior ? (
+            <Link
+              href={`/token/${contractAddress}/${nft.metadata.id}`}
+              key={nft.metadata.id}
+            >
+              
+              <NFTComponent nft={nft} />
+              
+            </Link>
+          ) : (
+            <div
+              key={nft.metadata.id}
+              onClick={() => overrideOnclickBehavior(nft)}
+            >
+              <NFTComponent nft={nft} />
+            </div>
+          ),
+        )}
+        </div>
+      ) : (
+        <>{emptyText}</>
+      )}
+    </div>
+  )
+}
