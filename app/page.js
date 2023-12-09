5ac862407cb1e9e2e471c16eb4cf990d90bbc11e
AdarshVMore <skullcrushermore@gmail.com>
2023-12-09 07:56:14 +0530

    Landing Page Done ✅

--- app/page.js
+++ app/page.js
@@ -1,8 +1,9 @@
 "use client";
+import { FaArrowRight } from "react-icons/fa6";
 
 import { useRouter } from "next/navigation";
 const ClientId = process.env.NEXT_PUBLIC_CLIENT_ID;
-
+import Spline from "@splinetool/react-spline";
 import Image from "next/image";
 
 import {
@@ -37,13 +38,14 @@ const Landing = () => {
   //Connect Wallet Theme
   const customeTheme = darkTheme({
     colors: {
-      modalBg:
-        "linear-gradient(to right, rgb(255, 228, 230), rgb(204, 251, 241))",
-      primaryText: "#555",
-      secondaryText: "#333333",
+      modalBg: "rgba(255, 255, 255, 0.1)",
+      primaryText: "white",
+      // backdropFilter: "blur(10px)",
+      secondaryText: "white",
       walletSelectorButtonHoverBg: "rgb(194, 207, 204)",
       separatorLine: "#888",
       borderColor: "#333333",
+      borderRadius: "100px",
     },
   });
 
@@ -52,31 +54,48 @@ const Landing = () => {
       {address ? (
         //Wallet Connected Page (Marketplace)
         router.push(`/profile/${address}`)
-
       ) : (
         //Wallet Not Connected (Landing Page)
-        <main className="flex h-screen items-center justify-center ">
-          <ConnectWallet
-            theme={customeTheme}
-            modalSize={"wide"}
-            btnTitle="ENTER GAME"
-            modalTitle="GameFT"
-            modalTitleIconUrl=""
-            welcomeScreen={() => {
-              return (
-                <div>
-                  <MediaRenderer
-                    src={
-                      "ipfs://QmT5ULF1ZQpsKUHmnWk7yBoPCnSYLQckAR9VRcEopFBiSN/game-box-gif.gif"
-                    }
-                    height={570}
-                    width={570}
-                    style={{ objectFit: "fill", objectPosition: "right" }}
-                  />
-                </div>
-              );
-            }}
-          />
+        <main className=" landing">
+          <div className="top">
+            <div className="circles"></div>
+            <div className="line"></div>
+            <p className="company-name">GameFT 6551</p>
+            <div className="line"></div> <div className="circles"></div>
+          </div>
+          <div className="bottom">
+            <div className="left">
+              <h1>
+                First-Ever <br /> Exclusive <span>erc-6551</span> <br />{" "}
+                Marketplace
+              </h1>
+              <p>Now bundle your In-game assets with your game identity</p>
+              <ConnectWallet
+                modalSize="wide"
+                className="connect-wallet-btn "
+                theme={customeTheme}
+                btnTitle="Enter Game"
+                modalTitle="GameFT"
+                modalTitleIconUrl="../public/assets/imgs/walletlogo.png"
+                welcomeScreen={() => {
+                  return (
+                    <div>
+                      {/* <MediaRenderer
+                        src={
+                          "ipfs://QmT5ULF1ZQpsKUHmnWk7yBoPCnSYLQckAR9VRcEopFBiSN/game-box-gif.gif"
+                        }
+                        width={570}
+                        style={{ objectFit: "fill", objectPosition: "right" }}
+                      /> */}
+                    </div>
+                  );
+                }}
+              />
+            </div>
+            <div className="right">
+              {/* <Spline scene="https://prod.spline.design/eHp77GJEs98xT8-4/scene.splinecode" /> */}
+            </div>
+          </div>
         </main>
       )}
     </div>
--- app/profile/[address]/page.jsx
+++ app/profile/[address]/page.jsx
@@ -1,49 +1,48 @@
-'use client'
+"use client";
 
-const ClientId = process.env.NEXT_PUBLIC_CLIENT_ID
-import { NFT_DROP_ADDRESS } from '@/app/constants/constant'
+const ClientId = process.env.NEXT_PUBLIC_CLIENT_ID;
+import { NFT_DROP_ADDRESS } from "@/app/constants/constant";
+import Navbar from "@/public/components/Navbar";
 
-import React from 'react'
+import React from "react";
 import {
   useContract,
   useOwnedNFTs,
   ThirdwebProvider,
   useAddress,
   Web3Button,
-} from '@thirdweb-dev/react'
+  ConnectWallet,
+} from "@thirdweb-dev/react";
 // import NFTData from '@/components/NFTData'
 // import Navbar from '@/components/Navbar'
 
-
-import { customSmartWallet, customSmartWallet2 } from "../../constants/walletConfig"
+import {
+  customSmartWallet,
+  customSmartWallet2,
+} from "../../constants/walletConfig";
 
 export default function Page() {
   return (
     <ThirdwebProvider
       activeChain={"polygon"}
       clientId={ClientId}
-      supportedWallets={[
-        customSmartWallet,
-        customSmartWallet2,
-      ]}
+      supportedWallets={[customSmartWallet, customSmartWallet2]}
     >
       <Address />
     </ThirdwebProvider>
-  )
+  );
 }
 
 const Address = () => {
-  const address = useAddress()
+  const address = useAddress();
 
-  const { contract: nftCollection } = useContract(
-    NFT_DROP_ADDRESS,
-  )
+  const { contract: nftCollection } = useContract(NFT_DROP_ADDRESS);
 
   const { data: ownedNfts, isLoading: loadingOwnedNfts } = useOwnedNFTs(
     nftCollection,
-    address,
-  )
-  console.log(ownedNfts)
+    address
+  );
+  console.log(ownedNfts);
   return address ? (
     <div>
       {/* <Navbar />
@@ -53,6 +52,7 @@ const Address = () => {
         address={address}
         emptyText={"You don't own any NFTs yet from this collection."}
       /> */}
+      <Navbar />
       <div className="flex flex-col justify-center items-center mt-3">
         <Web3Button
           contractAddress={NFT_DROP_ADDRESS}
@@ -60,6 +60,7 @@ const Address = () => {
         >
           Claim New NFT
         </Web3Button>
+        <ConnectWallet />
       </div>
     </div>
   ) : (
@@ -67,5 +68,5 @@ const Address = () => {
       {/* <Navbar /> */}
       <p>Connect Wallet</p>
     </div>
-  )
-}
+  );
+};
